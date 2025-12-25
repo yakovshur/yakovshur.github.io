77867dbfb4a9211bc095bb966aa966a3f98045cc
yakovshur <y_shur@ucsb.edu>
2025-12-25 14:40:10 -0800

    filled fencing tester project page and added dropdown navbar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-if (document.getElementById('my-work-link')) {
-  document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
-  })
-}
+// const myWorkLink = document.getElementById('my-work-link');
+// const ucsbProjects = document.getElementById('ucsb-projects');
+
+// if (myWorkLink && ucsbProjects) {
+//   myWorkLink.addEventListener('click', (e) => {
+//     e.preventDefault(); // stop default anchor jump
+//     ucsbProjects.scrollIntoView({ behavior: "smooth" });
+//   });
+// }
